64bac717b2a7c7b0ce8d96128312106863977196
unknown <ennyola45@gmail.com>
2026-07-17 16:31:04 +0100

    new branch

---
+++ src/pages/businessbanking/BusinessBanking.tsx
@@ -0,0 +1,17 @@
+import FAQ from "../../components/FAQ"
+import FindABranch from "../../components/FindABranch"
+import Cards from "../../quicklinks/Cards"
+import Overview from "./Overview"
+
+const BusinessBanking = () => {
+  return (
+    <div>
+      <Overview/>
+      <Cards/>
+      <FindABranch/>
+      <FAQ/>
+    </div>
+  )
+}
+
+export default BusinessBanking
